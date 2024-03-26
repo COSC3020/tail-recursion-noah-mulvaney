29254e5a066f6c54a9c8eca2db8c388e41892ed1
Noah Mulvaney <mulvaney.np@gmail.com>
2024-03-26 10:00:42 -0600

    Implement fibonacci with tail recursion

---
+++ code.js
@@ -0,0 +1,14 @@
+// COSC3020 Tail Recursion Exercise
+// Noah Mulvaney, nmulvane@uywo.edu
+// 26 Mar 2024
+
+// Fibonacci numbers with tail recursion
+
+function fibr(n, sum, old) {
+  if (n < 2) return sum;
+  return fibr(n - 1, sum + old, sum);
+}
+
+function fib(n) {
+  return fibr(n, 1, 1);
+}
